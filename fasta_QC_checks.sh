aac8f183bd2dbff99f8329fdfc666e4297c93234
krlala <krlala@usfca.edu>
2018-10-07 22:33:03 +0000

    Added shellcheck changes

--- fasta_QC_checks.sh
+++ fasta_QC_checks.sh
@@ -1,4 +1,3 @@
-
 #!bin/bash
 #using echo to print a line about the script's general purpose
 echo -e "\n\nThis is a log file-Output from preliminary quality control checks on selected fasta files"
@@ -14,23 +13,23 @@ lines,the number of sequences in the file, and the \nsequence identifier lines s
 #$@ allows us to take  user input to process
 for file in "$@"
 do
-  echo -e "\nOutput for "$file":\n"
+  echo -e "\nOutput for $file:\n"
  #storing value of du, which fetches the disk space occupied, -h for human readable form, and using 
 #cut to only get the value not the filename, and storing in in z.
-  z=$(du -h $file | cut -f 1)
-  echo -e "Size:"$z" \n\nFirst three lines:\n----------\n"
+  z=$(du -h "$file" | cut -f 1)
+  echo -e "Size:$z \n\nFirst three lines:\n----------\n"
 #head for first three lines of a file
-  head -3 $file
+  head -3 "$file"
   echo -e "\n Last three lines:\n--------\n"
 #tail for last three lines of the file
-  tail -3 $file
+  tail -3 "$file"
 # using grep to find instances of  >, and counting them with -c, storing that number in t.
-  t=$(grep -c ">" $file)
-  echo -e "\nNumber of sequences:"$t"\n"
+  t=$(grep -c ">" "$file")
+  echo -e "\nNumber of sequences:$t\n"
 #using a similar grep, and using  cut to extract just the genus and species. -d and the quotes after 
 # used to specify delimiter, -f for specifying which fields to be extracted, piping grep output to cut.
 #Piping that output to a simple sort sorts alphabetically.
   echo -e "Sequence identifier:\n"
-  grep ">" $file | cut -d " " -f 2-3 | sort
+  grep ">" "$file" | cut -d " " -f 2-3 | sort
   echo -e "\n###############################\n"
 done
